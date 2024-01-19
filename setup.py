f872cb40ce88c8c32a2be31cfa4b51c85ba767f2
Jake Hansen <statefromjakefarm44@gmail.com>
2024-01-19 00:37:29 -0800

    fix name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='bark',
+    name='suno-bark',
     version='0.1.5',
     packages=['bark'],
     install_requires=['encodec', 'funcy', 'tqdm', 'transformers']
